53fb16313ec626a04d439c9490304452e7f8b18a
g.yakovlev <g.yakovlev@YAR-KRISTA.AD>
2022-10-18 19:15:03 +0300

    chg - Поправил функции от кексы

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
-function getRandomInt(min, max) {
-    if (min >= max){
-        return 'Ошибка. минимум больше максимума!';
-    }
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+function getRandomPositiveInteger (a, b) {
+    const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+    const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+   
+    const result = Math.random() * (upper - lower + 1) + lower;
+    
+    return Math.floor(result);
   }
-
-  function truncate(str, maxlegth) {
-    if (str.length >= maxlegth) {
-        return alert(false);
-    }else{
-        return alert(true);
-    }
-}
+ 
+ function checkStringLength (string, length) {
+    return string.length <= length;
+  }
