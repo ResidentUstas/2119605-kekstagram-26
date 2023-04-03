282aea4076c000c130e940cd475ca43083b1abf6
ResidentUstas <33098546+ResidentUstas@users.noreply.github.com>
2023-04-03 20:17:32 +0300

    chg - разбиваю на модули, работа с шаблонами

---
+++ js/rendering.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+import {generateObjects} from './util.js';
+console.log('module up!');
+// import {descriptionsList, commentsList, nameList} from './data.js';
+
---
+++ js/util.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-constant-condition */
+import {descriptionsList, commentsList, nameList} from './data.js';
+const pictureArray = [];
+const occupPictureNumbers = [];
+const occupCommentNumbers = [];
+
+function getRandomPositiveInteger(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+// eslint-disable-next-line no-unused-vars
+function checkStringLength(string, length) {
+  return string.length <= length;
+}
+
+function getPictureNumber() {
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const picNumber = getRandomPositiveInteger(1, 1001);
+    if (!occupPictureNumbers.includes(picNumber)) {
+      occupPictureNumbers.push(picNumber);
+      return picNumber;
+    }
+  }
+}
+
+function getCommentNumber() {
+  while (true) {
+    const commentNumber = getRandomPositiveInteger(1, 1001);
+    if (!occupCommentNumbers.includes(commentNumber)) {
+      occupCommentNumbers.push(commentNumber);
+      return commentNumber;
+    }
+  }
+}
+
+function getComment() {
+  const commentObj = {
+    id: getCommentNumber(),
+    avatar: `img/avatar-${getRandomPositiveInteger(1, 7)}.svg`,
+    message: commentsList[getRandomPositiveInteger(0, commentsList.length - 1)],
+    name: nameList[getRandomPositiveInteger(0, nameList.length - 1)]
+  };
+
+  return commentObj;
+}
+
+function generateObjects() {
+  for (let i = 0; i < 25; i++) {
+    const picture =
+        {
+          id: getRandomPositiveInteger(1, 26),
+          url: `photos/${getPictureNumber()}.jpg`,
+          description: descriptionsList[getRandomPositiveInteger(0, descriptionsList.length - 1)],
+          likes: getRandomPositiveInteger(15, 201),
+          comments: getComment()
+        };
+
+    pictureArray.push(picture);
+  }
+}
+
+export { generateObjects };
+
